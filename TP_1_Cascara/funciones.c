ac44fbce53156f361306397863f99d3bccc810f2
enzitonicolas <enzocruz25.ec@gmail.com>
2017-09-07 01:46:07 -0300

    Calcu casi terminada

--- TP_1_Cascara/funciones.c
+++ TP_1_Cascara/funciones.c
@@ -1,3 +1,6 @@
+#define false 0
+#define true 1
+//Suma
 int sumar(int num1,int num2)
 {
     int result;
@@ -5,6 +8,7 @@ int sumar(int num1,int num2)
     return result;
 
 }
+//Resta
 int restar (int num1,int num2)
 {
     int result;
@@ -12,18 +16,21 @@ int restar (int num1,int num2)
     return result;
 
 }
+//Multiplicacion
 int multip(int num1,int num2)
 {
     int result;
     result=num1*num2;
     return result;
 }
+//Division
 float divid(float num1,float num2)
 {
-    int result;
+    float result;
     result=num1/num2;
     return result;
 }
+//Factorial
 float factorial(float num1)
 {
     float result;
@@ -32,6 +39,71 @@ float factorial(float num1)
         return 1;
     }
     result=num1*factorial(num1-1);
+    printf("El factorial es: %.0f\n", result);
     return result;
 
+}
+//Verificar
+int verificacion(int num1,int num2)
+{
+    if(num1 == 0 || num2 == 0)
+        return false;
+    else
+        return true;
+}
+//Verificar Suma
+void calcusumar(int num1,int num2)
+{
+    if (verificacion(num1,num2))
+    {
+        int result = sumar(num1,num2);
+        printf("La suma entre (A+B) es igual a:%d\n",result);
+    }
+    else
+    {
+        printf("Cargar los datos");
+    }
+}
+//Verificar resta
+void calcurestar(int num1,int num2)
+{
+    if (verificacion(num1,num2))
+    {
+        int result = restar(num1,num2);
+        printf("La resta entre (A-B) es igual a:%d\n",result);
+    }
+    else
+    {
+        printf("Cargar los datos");
+    }
+}
+//Verificar Division
+void calcudivid(int num1,int num2)
+{
+    if (verificacion(num1,num2))
+    {
+        float result = divid(num1,num2);
+        printf("La division entre (A/B) es igual a: %.2f\n",result);
+    }
+    else
+    {
+        printf("Cargar los datos");
+    }
+    if(num2==0)
+    {
+        printf("No se puede dividir por cero\n");
+    }
+}
+//Verificar multiplicacion
+void calcumultip(int num1,int num2)
+{
+    if (verificacion(num1,num2))
+    {
+        int result = multip(num1,num2);
+        printf("La multiplicacion entre (A*B) es igual a:%d\n",result);
+    }
+    else
+    {
+        printf("Cargar los datos");
+    }
 }
--- TP_1_Cascara/main.c
+++ TP_1_Cascara/main.c
@@ -7,7 +7,7 @@ int main()
     char seguir='s';
     int opcion=0;
     int num1,num2,result;
-
+    float resultFactorial;
     while(seguir=='s')
     {
         printf("1- Ingresar 1er operando (A=x)\n");
@@ -17,7 +17,7 @@ int main()
         printf("5- Calcular la division (A/B)\n");
         printf("6- Calcular la multiplicacion (A*B)\n");
         printf("7- Calcular el factorial (A!)\n");
-        printf("8- Calcular todas las operacione\n");
+        printf("8- Calcular todas las operaciones\n");
         printf("9- Salir\n");
         printf("Ingresa los dos numeros,luego elija una que operacion realizar...Si desea salir presione 9.\n");
         scanf("%d",&opcion);
@@ -27,33 +27,33 @@ int main()
             case 1:
                 printf("Ingresa el primer numero:");
                 scanf("%d",&num1);
-                printf("El número ingresado es: %d\n",num1);
+                printf("El numero ingresado es: %d\n",num1);
                 break;
             case 2:
                 printf("Ingrese el segundo numero:");
                 scanf("%d",&num2);
-                printf("El segundo número ingresado es: %d\n",num2);
+                printf("El segundo numero ingresado es: %d\n",num2);
                 break;
             case 3:
-                sumar(num1,num2);
+                calcusumar(num1,num2);
                 break;
             case 4:
-               restar(num1,num2);
+               calcurestar(num1,num2);
                 break;
             case 5:
-               divid(num1,num2);
+               calcudivid(num1,num2);
                 break;
             case 6:
-               multip(num1,num2);
+               calcumultip(num1,num2);
                 break;
             case 7:
                 factorial(num1);
                 break;
             case 8:
-                sumar(num1,num2);
-                restar(num1,num2);
-                divid(num1,num2);
-                multip(num1,num2);
+                calcusumar(num1,num2);
+                calcurestar(num1,num2);
+                calcudivid(num1,num2);
+                calcumultip(num1,num2);
                 factorial(num1);
                 break;
             case 9:
@@ -62,6 +62,6 @@ int main()
         }
 
 
-    
+
 } return 0;
 }
